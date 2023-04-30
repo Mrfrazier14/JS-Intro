930d7a10295c5fcfa15412242b739b1857cb0e45
Aaron Frazier Jr <afraz91@wgu.edu>
2023-04-30 00:26:48 -0700

    Create main.js

---
+++ main.js
@@ -0,0 +1,12 @@
+// use the * operator to multiply 
+var multiply = 10*10;
+
+// use the / operator to divide
+var divide = 10 / 5;
+
+// operations on integers can result in floating point values, and vice versa
+var float = 10 / 3;
+
+console.log("Ten times ten equals", multiply);
+console.log("Ten divided by five equals", divide);
+console.log("Ten divided by three equals", float);
